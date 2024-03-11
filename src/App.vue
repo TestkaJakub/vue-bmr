34568c7cb46f4e9d1f02c73683c6a9c9e4895f1b
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2024-03-11 19:04:20 +0100

    theme switcher

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ThemeSwitch from './components/ThemeSwitch.vue';
+</script>
+
+<template>
+  <ThemeSwitch/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const root = document.querySelector(':root');
+
+const currentTheme = ref(localStorage.getItem('theme') || 'dark-theme');
+if (currentTheme.value === 'light-theme') root?.classList.add('light-theme');
+
+function changeTheme()
+{
+  if (root){
+    root.classList.toggle('light-theme');
+
+    currentTheme.value = root.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
+    localStorage.setItem('theme', currentTheme.value);
+  }
+}
+</script>
+<template>
+  <button @click="changeTheme()">
+    {{ currentTheme == 'light-theme' ? '🌑' : '☀️'}}
+  </button>
+</template>
+<style scoped>
+    button {
+        padding: 20px 20px;
+    }
+</style>
